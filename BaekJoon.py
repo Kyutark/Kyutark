f73f11ebfd4c41d29dea96df1ab8af98b5cc48fe
unknown <kyutkim01@gmail.com>
2026-01-27 16:39:45 +0900

    Typo Correction

--- BaekJoon.py
+++ BaekJoon.py
@@ -1,5 +1,18 @@
-N, k = map(int,input().split())
-num_list = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-num_list.sort()
-print(num_list[-k])
+N = int(input())
+
+ans = [0,1,2,4]
+q = []
+
+for _ in range(N):
+    q.append(int(input()))
+
+M = max(q)
+
+for i in range(4, M+1):
+    ans.append(ans[i-1] + ans[i-2] + ans[i-3])
+
+for i in q:
+    print(ans[i])
